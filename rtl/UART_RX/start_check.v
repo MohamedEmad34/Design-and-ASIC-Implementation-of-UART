cca362c3f7e785ca6fcc87f977cb6a93028284c2
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2026-08-03 12:00:00 +0300

    Update start_check.v

--- rtl/UART_RX/start_check.v
+++ rtl/UART_RX/start_check.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2026 02:07:58 PM
+// Design Name: 
+// Module Name: start_check
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module start_check(
+
+    input  wire CLK,
+    input  wire RST,
+    input  wire Enable,
+    input  wire sampled_bit,
+    output reg  strt_glitch
+
+);
+always @(posedge CLK or negedge RST)
+begin
+    if(!RST)
+        strt_glitch <= 1'b0;
+
+    else if(Enable)
+        strt_glitch <= sampled_bit;
+     else
+     strt_glitch<=1'b0;
+end
+endmodule

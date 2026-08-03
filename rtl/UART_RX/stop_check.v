3edbbcc670c766f94ae4c1cefc5f97da811e90cf
Mohamed_Emad <102392237+MohamedEmad34@users.noreply.github.com>
2026-08-03 12:00:36 +0300

    Update stop_check.v

--- rtl/UART_RX/stop_check.v
+++ rtl/UART_RX/stop_check.v
@@ -1,1 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2026 03:05:02 PM
+// Design Name: 
+// Module Name: stop_check
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+module stop_check(
+
+    input  wire CLK,
+    input  wire RST,
+
+    input  wire Enable,
+
+    input  wire sampled_bit,
+
+    output reg stp_err
+
+);
+always @(posedge CLK or negedge RST)
+begin
+
+    if(!RST)
+        stp_err <= 1'b0;
+
+    else if(Enable)
+        stp_err <= ~sampled_bit;
+
+    else
+        stp_err <= 1'b0;
+
+end
+endmodule
